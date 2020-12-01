e30ebe21558c6f58dad78b066b03de297c8a9ccd
Mitchell Tuinstra <24893@ma-web.nl>
2020-12-01 14:20:41 +0100

    finished Day 1 Part 1

---
+++ Day 1/part one/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace part_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lineList = new List<int>();
+            var lines = File.ReadAllLines("input.txt");
+            foreach(string s in lines)
+            {
+                lineList.Add(Convert.ToInt32(s));
+            }
+
+            foreach(int intA in lineList)
+            {
+                foreach(int intB in lineList)
+                {
+                    if((intA + intB) == 2020)
+                    {
+                        PrintFlag(intA, intB);
+                    }
+                }
+            }
+        }
+
+        static void PrintFlag(int intA, int intB)
+        {
+            Console.WriteLine(intA * intB);
+        }
+
+    }
+}
